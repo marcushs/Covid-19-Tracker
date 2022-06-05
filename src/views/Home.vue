41328de45e1f49278bdc3565ac26a39c528584ad
Marcus Hok Sum Leung <marcus.hoksum.leung@gmail.com>
2022-06-05 20:02:46 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    Hello world
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const url = 'https://api.covid19api.com/summary'
+
+export default {
+  name: 'Home',
+  components: {},
+  methods: {
+    async fetchCovidData() {
+      const {data} = await axios.get(url)
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    console.log(data)
+  }
+}
+</script>
