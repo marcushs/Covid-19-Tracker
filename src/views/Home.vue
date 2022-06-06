c5e5e87f2c06e11f8d328ba40d5299f3ac754c53
Marcus Hok Sum Leung <marcus.hoksum.leung@gmail.com>
2022-06-06 22:21:45 +0200

    Covid-19 tracker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,71 @@
 <template>
-  <div class="home">
-    Hello world
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+    <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button @click="clearCountryData"
+    v-if="stats.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
+      Clear Country
+    </button>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 mt-6 mx-auto" alt="">
+  </main>
 </template>
 
 <script>
 import axios from 'axios'
+import DataTitle from '@/components/DataTitle.vue'
+import DataBoxes from '@/components/DataBoxes.vue'
+import CountrySelect from '@/components/CountrySelect.vue'
+
 const url = 'https://api.covid19api.com/summary'
 
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '', 
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
   methods: {
     async fetchCovidData() {
       const {data} = await axios.get(url)
       return data
+    },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
+    },
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
     }
   },
   async created() {
     const data = await this.fetchCovidData()
-    console.log(data)
+    
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
   }
 }
 </script>
